a19bb6ee9487909171cb9ab3078340f6f6908658
dave <david.bethke@gmail.com>
2024-05-17 18:01:39 -0500

    update manifest

--- addons/mission2/__manifest__.py
+++ addons/mission2/__manifest__.py
@@ -18,7 +18,7 @@ pre, post, in-flight plan
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Human Resources/Employees',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -26,7 +26,7 @@ pre, post, in-flight plan
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
@@ -34,5 +34,8 @@ pre, post, in-flight plan
     'demo': [
         'demo/demo.xml',
     ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
 }
 
