e9c5cb32c0a8ca8a254288f17170547e4f02b7a3
dave <david.bethke@gmail.com>
2024-06-12 13:47:27 -0500

    add some menus

--- addons/mission2/__manifest__.py
+++ addons/mission2/__manifest__.py
@@ -19,7 +19,7 @@ pre, post, in-flight plan
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Human Resources/Mission',
-    'version': '0.825',
+    'version': '0.828',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
